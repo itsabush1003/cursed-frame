a4b6c8b61d095164e0c9ab9ffa84815db6a99800
itsabush1003 <11869471+itsabush1003@users.noreply.github.com>
2026-03-27 10:59:51 +0900

    Add Unity frontend

---
+++ frontend/Unity/CursedFrame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyObject = null;
+    [SerializeField] private Material profileImageMaterial = null;
+    [SerializeField] private CameraController cameraController;
+    [SerializeField] private MagiciansController magiciansController;
+    [SerializeField] private int debugTeamId = 0;
+    private MessageLiaison liaison = null;
+    private int currentTargetTeamId = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool alreadyPrepared = liaison != null;
+        liaison = GameObject.FindWithTag("MessageLiaison")?.GetComponent<MessageLiaison>();
+        enemyObject.transform.DOMove(Vector3.up * 0.3f, 2.0f).SetRelative(true).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+# if UNITY_EDITOR
+        if (!alreadyPrepared) StartPrepareAnimation();
+# endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+# if UNITY_EDITOR
+        if(Input.GetMouseButtonDown(0))
+        {
+            cameraController.StartAttentionAnimation();
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            ChangeTargetTeam(4);
+        }
+        if(Input.GetMouseButtonDown(2))
+        {
+            StartAttackAnimation(new bool[] {false, false, true, false, true, true, true, false, true});
+        }
+# endif
+    }
+
+    /// <summary>
+    /// MessageLiaisonが自身をGameManagerに登録するための関数
+    /// OnSceneLoadedがStartより前に実行されてしまうので、その対策
+    /// </summary>
+    /// <param name="messageLiaison">MessageLiaisonのインスタンス</param>
+    public void RegistMessageLiaison(MessageLiaison messageLiaison)
+    {
+        liaison = messageLiaison;
+    }
+
+    /// <summary>
+    /// 開始準備のアニメーションを再生する関数
+    /// </summary>
+    /// <param name="callback">アニメーション終了時に呼ばれるコールバック関数</param>
+    public void StartPrepareAnimation(Action callback = null)
+    {
+        enemyObject.transform.DOPause();
+        Transform target = enemyObject.transform.Find("EnemyGroundPosition");
+        int teamId = liaison?.GetTeamId() ?? debugTeamId;
+        int teamNum = liaison?.GetTeamNum() ?? MagiciansController.MAX_CHARA_NUM;
+        magiciansController.StartPrepareAnimation(teamId, teamNum, target, () =>
+        {
+            if (0 < teamId && teamId <= MagiciansController.MAX_CHARA_NUM)
+            {
+                enemyObject.transform.LookAt(magiciansController.GetMagician(teamId).transform.position + Vector3.up * enemyObject.transform.position.y);
+            }
+            enemyObject.transform.DOPlay();
+        });
+        if (teamId == 0)
+        {
+            cameraController.StartPrepareAnimation(CameraController.TweenTarget.Admin, 0.0f, callback);
+        }
+        else if (0 < teamId && teamId <= MagiciansController.MAX_CHARA_NUM)
+        {
+            float angle = Mathf.Atan2(magiciansController.GetMagician(teamId).transform.localPosition.x, 2.0f) * Mathf.Rad2Deg;
+            cameraController.StartPrepareAnimation(CameraController.TweenTarget.Guest, angle, callback);
+        }
+    }
+
+    /// <summary>
+    /// 次のターゲットを表示・強調するアニメーションを再生する関数
+    /// </summary>
+    /// <param name="texture">Enemyに読み込ませる画像用テクスチャ</param>
+    /// <param name="callback">アニメーション終了時に呼ばれるコールバック関数</param>
+    public void SetNextTarget(Texture2D texture, Action callback)
+    {
+        enemyObject.transform.DOPause();
+        ChangeEnemyTexture(texture);
+        cameraController.StartAttentionAnimation(() => {
+            enemyObject.transform.DOPlay();
+            callback?.Invoke();
+        });
+    }
+
+    /// <summary>
+    /// ターゲットとなるチームが変わった事を示すアニメーションを再生する関数
+    /// </summary>
+    /// <param name="teamId">次のターゲットになるチームのID</param>
+    /// <param name="callback">アニメーション終了時に呼ばれるコールバック関数</param>
+    public void ChangeTargetTeam(int teamId, Action callback = null)
+    {
+        int myTeamId = liaison?.GetTeamId() ?? debugTeamId;
+        GameObject target = magiciansController.GetMagician(teamId);
+        enemyObject.transform.DOPause();
+        Transform originalPosition = enemyObject.transform;
+        Sequence sequence = DOTween.Sequence()
+            .Append(enemyObject.transform.DOShakeRotation(1.0f, new Vector3(5.0f, 5.0f, 30.0f), 10, 90.0f));
+        if (currentTargetTeamId == myTeamId)
+        {
+            sequence.Join(cameraController.ReleaseAnimation());
+        }
+        sequence.AppendInterval(1.0f)
+            .Append(enemyObject.transform.DOLookAt(target.transform.position + Vector3.up * enemyObject.transform.position.y, 1.0f))
+            .Append(enemyObject.transform.DOMove(target.transform.position + Vector3.right * enemyObject.transform.localScale.x / 2 * Mathf.Sign(target.transform.localPosition.x) * -1 + Vector3.up + enemyObject.transform.forward, 3.0f).SetEase(Ease.InOutBack, 2.0f))
+            .AppendInterval(0.5f);
+        if (teamId == myTeamId)
+        {
+            sequence.Join(cameraController.CaptureAnimation(originalPosition.position));
+        }
+        sequence.Append(enemyObject.transform.DOMove(originalPosition.position, 3.0f))
+            .Join(enemyObject.transform.DORotateQuaternion(originalPosition.rotation, 3.0f))
+            .OnComplete(() => {
+                currentTargetTeamId = teamId;
+                enemyObject.transform.DOPlay();
+                callback?.Invoke();
+            });
+    }
+
+    /// <summary>
+    /// 攻撃アニメーションを再生する関数
+    /// </summary>
+    /// <param name="isCorrects">各チームの正解・不正解が入った配列　個人用の場合はそのチームの要素一つだけ</param>
+    /// <param name="callback">アニメーション終了時に呼ばれるコールバック関数</param>
+    public void StartAttackAnimation(bool[] isCorrects, Action callback = null)
+    {
+        magiciansController.StartAttackAnimation(liaison?.GetTeamId() ?? debugTeamId, currentTargetTeamId, enemyObject.transform.position + Vector3.right, isCorrects);
+    }
+
+    /// <summary>
+    /// Enemyの画像テクスチャを指定したテクスチャに変更する関数
+    /// </summary>
+    /// <param name="tex">読み込む画像テクスチャ</param>
+    private void ChangeEnemyTexture(Texture2D tex)
+    {
+        profileImageMaterial.mainTexture = tex;
+        profileImageMaterial.color = Color.white;
+    }
+}
---
+++ frontend/Unity/CursedFrame/Assets/Scripts/MagiciansController.cs
@@ -0,0 +1,223 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class MagiciansController : MonoBehaviour
+{
+    public const int MAX_CHARA_NUM = 9;
+
+    [SerializeField] private GameObject[] magicians = new GameObject[MAX_CHARA_NUM];
+    [SerializeField] private GameObject[] bullets = new GameObject[MAX_CHARA_NUM];
+    [SerializeField] private Animator[] animators = new Animator[MAX_CHARA_NUM];
+    [SerializeField] private ParticleSystem particleSystem;
+
+    private MaterialPropertyBlock propertyBlock = null;
+    private readonly Vector3 positionOffset = new Vector3(-0.5f, 0.0f, 0.0f);
+    private readonly Vector3 basePosition = new Vector3(0.0f, 0.0f, 2.0f);
+    private readonly Color[] colors = new Color[MAX_CHARA_NUM]
+    {
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        Color.cyan,
+        Color.magenta,
+        Color.white,
+        Color.gray,
+        Color.black
+    };
+
+    void Awake()
+    {
+        propertyBlock  = new MaterialPropertyBlock();
+        particleSystem = GetComponent<ParticleSystem>();
+        for (var i = 0; i < magicians.Length; i++)
+        {
+            bullets[i] = magicians[i].transform.Find("Bullet").gameObject;
+            animators[i] = magicians[i].GetComponent<Animator>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// キャラクタのGameObjectを取得する関数
+    /// </summary>
+    /// <param name="teamId">何番目のキャラクタか</param>
+    /// <returns></returns>
+    public GameObject GetMagician(int teamId)
+    {
+        return magicians[teamId-1];
+    }
+
+    /// <summary>
+    /// クエスト開始時のアニメーションを再生する関数
+    /// </summary>
+    /// <param name="teamId">何番目のキャラクタを表示するか　複数体表示したい場合は0を入れる</param>
+    /// <param name="teamNum">teamId==0のとき、全部で何体のキャラを表示するか</param>
+    /// <param name="target">キャラクタが視線を向ける対象</param>
+    /// <param name="callback">アニメーション終了時に呼ばれるコールバック関数</param>
+    public void StartPrepareAnimation(int teamId, int teamNum, Transform target, System.Action callback = null)
+    {
+        if (teamId == 0)
+        {
+            for (int i = 0; i < magicians.Length && i < teamNum; i++)
+            {
+                if (teamNum % 2 == 0) magicians[i].transform.localPosition += positionOffset;
+                SetupMagician(magicians[i], target, i < colors.Length ? colors[i] : Color.clear);
+                animators[i].SetBool("isRunning", true);
+            }
+            moveToBase().OnComplete(() => {
+                for (int i = 0; i < magicians.Length && i < teamNum; i++)
+                {
+                    animators[i].SetBool("isRunning", false);
+                }
+                particleSystem.Stop();
+                callback?.Invoke();
+            });
+        }
+        else if (0 < teamId && teamId <= magicians.Length)
+        {
+            magicians[teamId-1].transform.localPosition /= 2;
+            var sh = particleSystem.shape;
+            sh.position += magicians[teamId-1].transform.localPosition;
+            sh.scale = Vector3.one;
+            float angle = Mathf.Atan2(magicians[teamId-1].transform.localPosition.x, basePosition.z) * Mathf.Rad2Deg;
+            SetupMagician(magicians[teamId-1], target, colors[teamId-1]);
+            animators[teamId-1].SetBool("isRunning", true);
+            DOTween.Sequence()
+                .Append(moveToBase())
+                .Join(magicians[teamId-1].transform.DOLookAt(target.position, 2.0f).SetDelay(2.0f))
+                .OnComplete(() => {
+                    animators[teamId-1].SetBool("isRunning", false);
+                    particleSystem.Stop();
+                    callback?.Invoke();
+                });
+        }
+    }
+
+    /// <summary>
+    /// 攻撃用アニメーションを再生する関数
+    /// </summary>
+    /// <param name="teamId">アニメーションを適用するチームのID</param>
+    /// <param name="unAttackTeamId">攻撃できないチームのID</param>
+    /// <param name="target">攻撃を当てるターゲットのワールド座標</param>
+    /// <param name="isSuccesses">攻撃が成功するかどうかが入った配列　teamId==0の時はチーム数分、1<teamId<teamNumの時は一つだけ</param>
+    public void StartAttackAnimation(int teamId, int unAttackTeamId, Vector3 target, bool[] isSuccesses)
+    {
+        if (teamId == 0)
+        {
+            for (var i = 0; i < bullets.Length; i++)
+            {
+                if (i+1 == unAttackTeamId) continue;
+                SetupBulletAnimation(bullets[i], isSuccesses[i] ? target : transform.position);
+            }
+        }
+        else if (0 < teamId && teamId <= MAX_CHARA_NUM && teamId != unAttackTeamId)
+        {
+            SetupBulletAnimation(bullets[teamId-1], isSuccesses[0] ? target : transform.position);
+        }
+    }
+
+    /// <summary>
+    /// キャラクタの初期設定をする関数
+    /// </summary>
+    /// <param name="magician">初期設定するキャラクタのGameObject</param>
+    /// <param name="target">GameObjectが向くべき対象</param>
+    /// <param name="color">モデルの色</param>
+    private void SetupMagician(GameObject magician, Transform target, Color color)
+    {
+        magician.SetActive(true);
+        magician.transform.LookAt(target);
+        Renderer[] renderers = magician.GetComponentsInChildren<Renderer>();
+        if (renderers != null)
+        {
+            foreach (Renderer renderer in renderers)
+            {  
+                // 弾は最初ActiveでないとGetComponentsに引っかからないので、取得後に非表示にする
+                if (renderer.gameObject.name == "Energy") renderer.transform.parent.gameObject.SetActive(false);
+                renderer.GetPropertyBlock(propertyBlock);
+                propertyBlock.SetColor("_Color", color);
+                // 完全な黒だと弾が見えないので、少しだけ色を付ける
+                // それ以外の色の場合は、透明度を半分にする
+                propertyBlock.SetColor("_TintColor", color == Color.black ? new Color(0.1f, 0.1f, 0.1f, 0.5f) : color - new Color(0.0f, 0.0f, 0.0f, 0.5f));
+                renderer.SetPropertyBlock(propertyBlock);
+            }
+        }
+    }
+
+    /// <summary>
+    /// bulletのアニメーションを再生する関数
+    /// </summary>
+    /// <param name="bullet">bulletのGameObject</param>
+    /// <param name="target">bulletを当てるワールド座標</param>
+    private void SetupBulletAnimation(GameObject bullet, Vector3 target)
+    {
+        Vector3 originalPosition = bullet.transform.position;
+        Vector3 originalScale = bullet.transform.localScale;
+        Renderer energyRenderer = bullet.transform.Find("Energy").gameObject.GetComponent<Renderer>();
+        Color originalColor = GetColorWithPB(energyRenderer);
+        bullet.SetActive(true);
+        DOTween.Sequence()
+            .AppendInterval(1.0f)
+            .Append(bullet.transform.DOMove(target, 2.0f))
+            .Append(DOTween.To(() => originalColor.a, x => SetColorWithPB(energyRenderer, new Color(originalColor.r,originalColor.g,originalColor.b,x)), 0.0f, 1.0f).SetEase(Ease.OutExpo))
+            // .Append(energyRenderer.materal.DOFade(0.0f, "_TintColor", 1.0f).SetEase(Ease.OutExpo))
+            .Join(bullet.transform.DOScale(new Vector3(2.0f, 2.0f, 1.0f), 1.0f).SetEase(Ease.OutExpo))
+            .OnComplete(() =>
+            {
+                bullet.SetActive(false);
+                bullet.transform.position = originalPosition;
+                bullet.transform.localScale = originalScale;
+                SetColorWithPB(energyRenderer, originalColor);
+            });
+    }
+
+    /// <summary>
+    /// MaterialPropertyBlock経由で色を取得する関数
+    /// </summary>
+    /// <param name="renderer">色を取得したいマテリアルを持つRenderer</param>
+    /// <param name="property">プロパティ名</param>
+    /// <returns>プロパティに設定されているColor</returns>
+    private Color GetColorWithPB(Renderer renderer, string property = "_TintColor")
+    {
+        renderer.GetPropertyBlock(propertyBlock);
+        Color color = propertyBlock.GetColor(property);
+        renderer.SetPropertyBlock(propertyBlock);
+        return color;
+    }
+
+    /// <summary>
+    /// MaterialPropertyBlock経由で色を設定する関数
+    /// </summary>
+    /// <param name="renderer">色を設定したいマテリアルを持つRenderer</param>
+    /// <param name="color">設定したい色</param>
+    /// <param name="property">プロパティ名</param>
+    private void SetColorWithPB(Renderer renderer, Color color, string property = "_TintColor")
+    {
+        renderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor(property, color);
+        renderer.SetPropertyBlock(propertyBlock);
+    }
+
+    /// <summary>
+    /// キャラクタを初期位置に移動するアニメーションを返す（再生する）関数
+    /// </summary>
+    /// <returns>Tweenアニメーション</returns>
+    private Tweener moveToBase()
+    {
+        return transform.DOMove(basePosition, 4.0f);
+    }
+}
